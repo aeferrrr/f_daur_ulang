df1fe218199d1dd6d85b25e0e0c7855416b7f266
Arief Fadhillah R <arief.fadhillah.r@gmail.com>
2023-08-26 20:01:43 +0700

    can't delete infochannel

--- lib/vidio_lainnya.dart/screens/home_screen.dart
+++ lib/vidio_lainnya.dart/screens/home_screen.dart
@@ -163,6 +163,9 @@ class _HomeScreenState extends State<HomeScreen> {
               child: ListView.builder(
                 itemCount: 1 + _channel.videos.length,
                 itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return _buildProfileInfo();
+                  }
                   Video video = _channel.videos[index - 1];
                   return _buildVideo(video);
                 },
