5a752f0a347ecfcf9021613d2020de82ec741d57
rdwnrvldi <ridwanrivaldi17@gmail.com>
2023-08-28 19:59:53 +0700

    add page profile

--- lib/anorganik/anorganik_page.dart
+++ lib/anorganik/anorganik_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_daur_ulang/anorganik/botol_page.dart';
 
+
 class AnorganikPage extends StatefulWidget {
   const AnorganikPage({super.key});
 
@@ -15,8 +16,8 @@ class _AnorganikPageState extends State<AnorganikPage> {
       backgroundColor: Colors.blue[800],
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
       body: SafeArea(
@@ -174,7 +175,7 @@ class _AnorganikPageState extends State<AnorganikPage> {
             ),
 
             SizedBox(
-              height: 25,
+              height: 16,
             ),
             // CONTAINER UTAMA
             Expanded(
